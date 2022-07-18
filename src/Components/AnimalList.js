dcd53cd330782640fd9470e00b547bc32bebe079
Amir <amir.zahari@besquare.com.my>
2022-07-18 15:02:42 +0800

    add comments

--- src/Components/AnimalList.js
+++ src/Components/AnimalList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./index.css";
 
+//Animal List
 class AnimalList extends Component {
   constructor(props) {
     super(props);
